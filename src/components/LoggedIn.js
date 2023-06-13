0f80ea24d63ef6f65b0f2be8c90dead5a103e830
Chris Boydstun <chrisboydstun@gmail.com>
2023-06-12 21:26:09 -0500

    created loggedin component

---
+++ src/components/LoggedIn.js
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+
+export default function LoggedIn() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  return (
+    <div>
+      {isLoggedIn ? <h1>Welcome</h1> : <h1>Please Login</h1>}
+      
+      
+      <button onClick={() => setIsLoggedIn(!isLoggedIn)}>
+        {isLoggedIn ? "Logout" : "Login"}
+      </button>
+    </div>
+  );
+}
